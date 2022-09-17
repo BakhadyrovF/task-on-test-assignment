b0f8645771618646f93dc1ed6da0c136c1dc2c17
BakhadyrovF <BakhadyrovF@gmail.com>
2022-09-17 10:38:45 +0500

    feat: endpoint for getting list of products

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
     public function __invoke()
     {
         $products = Product::with('warehouses')
             ->orderByDesc('manufacture_date')->get();
 
+        return ProductResource::collection($products);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/products', )
+Route::get('/products', [\App\Http\Controllers\Api\ProductController::class, '__invoke']);
